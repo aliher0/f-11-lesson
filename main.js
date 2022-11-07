7356cca257078058fca7fb153545960263f1cb86
aliher0 <alisertolgoev434@gmail.com>
2022-11-07 10:37:29 +0300

    приступули к проекту todolist

---
+++ main.js
@@ -0,0 +1,34 @@
+// git github
+// setTimeOut() - setInterval()
+// Project - Todo List
+
+// function test () {
+
+// }
+
+// setTimeout(() => {
+//     console.log('Test')
+// },  3000 )
+
+
+// setInterval(() => {
+//     console.log('setlnterval')
+// }, 3000)
+
+
+const TodoLisTask = [];
+
+
+var todoListAddBtn = document.getElementById
+('todo-list-task-add-bth')
+
+
+
+
+todoListAddBtn.addEventListener('click')
+
+function AddTodo(event) {
+    var todoListTaskText = document.getElementById
+    ('todo-list-task-text').value;
+    console.                   log(todoListTaskText )
+}
